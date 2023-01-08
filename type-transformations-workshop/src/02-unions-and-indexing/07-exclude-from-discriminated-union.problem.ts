165e6f8474826de68a5881d6e354c1a85d79cba5
RafaZeero <rafaa99@gmail.com>
2023-01-08 07:12:00 -0300

    update total ts

--- type-transformations-workshop/src/02-unions-and-indexing/07-exclude-from-discriminated-union.problem.ts
+++ type-transformations-workshop/src/02-unions-and-indexing/07-exclude-from-discriminated-union.problem.ts
@@ -1,27 +1,22 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 export type Event =
   | {
-      type: "click";
-      event: MouseEvent;
+      type: 'click'
+      event: MouseEvent
     }
   | {
-      type: "focus";
-      event: FocusEvent;
+      type: 'focus'
+      event: FocusEvent
     }
   | {
-      type: "keydown";
-      event: KeyboardEvent;
-    };
+      type: 'keydown'
+      event: KeyboardEvent
+    }
 
-type NonKeyDownEvents = unknown;
+type NonKeyDownEvents = MyExclude<Event, { type: 'click' } | { type: 'focus' }>
+type MyExclude<T, P> = T extends P ? never : T
 
 type tests = [
-  Expect<
-    Equal<
-      NonKeyDownEvents,
-      | { type: "click"; event: MouseEvent }
-      | { type: "focus"; event: FocusEvent }
-    >
-  >,
-];
+  Expect<Equal<NonKeyDownEvents, { type: 'click'; event: MouseEvent } | { type: 'focus'; event: FocusEvent }>>,
+]
