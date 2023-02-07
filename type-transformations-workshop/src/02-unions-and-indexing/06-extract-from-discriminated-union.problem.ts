22227db001c5486b953c309857e6e0d7c136efe5
RafaZeero <rafaa99@gmail.com>
2023-02-07 17:33:55 -0300

    solve extract from discriminated

--- type-transformations-workshop/src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
+++ type-transformations-workshop/src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
@@ -1,19 +1,21 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 export type Event =
   | {
-      type: "click";
-      event: MouseEvent;
+      type: 'click'
+      event: MouseEvent
     }
   | {
-      type: "focus";
-      event: FocusEvent;
+      type: 'focus'
+      event: FocusEvent
     }
   | {
-      type: "keydown";
-      event: KeyboardEvent;
-    };
+      type: 'keydown'
+      event: KeyboardEvent
+    }
 
-type ClickEvent = unknown;
+type ClickEvent = MyExtract<Event, { type: 'click' }>
 
-type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
+type MyExtract<T, K> = T extends K ? T : never
+
+type tests = [Expect<Equal<ClickEvent, { type: 'click'; event: MouseEvent }>>]
