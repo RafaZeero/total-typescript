8680635f1d9909b01b246b25b097ab5210d16606
RafaZeero <rafaa99@gmail.com>
2023-03-09 00:03:54 -0300

    feat: solve get object values

--- type-transformations-workshop/src/02-unions-and-indexing/12-get-object-values.problem.ts
+++ type-transformations-workshop/src/02-unions-and-indexing/12-get-object-values.problem.ts
@@ -1,15 +1,12 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const frontendToBackendEnumMap = {
-  singleModule: "SINGLE_MODULE",
-  multiModule: "MULTI_MODULE",
-  sharedModule: "SHARED_MODULE",
+  singleModule: 'SINGLE_MODULE',
+  multiModule: 'MULTI_MODULE',
+  sharedModule: 'SHARED_MODULE',
 } as const;
 
-type BackendModuleEnum = unknown;
+type Obj = typeof frontendToBackendEnumMap;
+type BackendModuleEnum = Obj[keyof Obj];
 
-type tests = [
-  Expect<
-    Equal<BackendModuleEnum, "SINGLE_MODULE" | "MULTI_MODULE" | "SHARED_MODULE">
-  >,
-];
+type tests = [Expect<Equal<BackendModuleEnum, 'SINGLE_MODULE' | 'MULTI_MODULE' | 'SHARED_MODULE'>>];
