929780f6edf704cb7077d593f015428f131170b4
RafaZeero <rafaa99@gmail.com>
2023-03-15 14:26:29 -0300

    solve template literal value extraction

--- type-transformations-workshop/src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
+++ type-transformations-workshop/src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
@@ -1,19 +1,16 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type Names = [
-  "Matt Pocock",
-  "Jimi Hendrix",
-  "Eric Clapton",
-  "John Mayer",
-  "BB King",
-];
+type Names = ['Matt Pocock', 'Jimi Hendrix', 'Eric Clapton', 'John Mayer', 'BB King'];
 
-type GetSurname<T> = unknown;
+/**
+ * Type helper extends two values ? return last value, else never
+ */
+type GetSurname<T> = T extends `${infer F} ${infer L}` ? L : never;
 
 type tests = [
-  Expect<Equal<GetSurname<Names[0]>, "Pocock">>,
-  Expect<Equal<GetSurname<Names[1]>, "Hendrix">>,
-  Expect<Equal<GetSurname<Names[2]>, "Clapton">>,
-  Expect<Equal<GetSurname<Names[3]>, "Mayer">>,
-  Expect<Equal<GetSurname<Names[4]>, "King">>,
+  Expect<Equal<GetSurname<Names[0]>, 'Pocock'>>,
+  Expect<Equal<GetSurname<Names[1]>, 'Hendrix'>>,
+  Expect<Equal<GetSurname<Names[2]>, 'Clapton'>>,
+  Expect<Equal<GetSurname<Names[3]>, 'Mayer'>>,
+  Expect<Equal<GetSurname<Names[4]>, 'King'>>,
 ];
