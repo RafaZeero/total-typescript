c20e8d8559a4dcc8bb1eedcb2ab724b8bddb7f8d
RafaZeero <rafaa99@gmail.com>
2023-03-15 14:26:08 -0300

    solve infer with raw values

--- type-transformations-workshop/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ type-transformations-workshop/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,14 +1,15 @@
-import { Equal, Expect } from "../helpers/type-utils";
-
-type GetDataValue<T> = unknown;
+import { Equal, Expect } from '../helpers/type-utils';
+/**
+ * Solution 01:  
+ * Type helper <T> can be any, then if <T> extends { data: any }, return `T['data']`, else `never`
+ *
+ * Solution 02:  
+ * Type helper <T> can be any, then if <T> extends { data: `infer D`}, return `D`, else `never`
+ */
+type GetDataValue<T> = T extends { data: any } ? T['data'] : never;
 
 type tests = [
-  Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
-  Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
-  Expect<
-    Equal<
-      GetDataValue<{ data: { name: "hello"; age: 20 } }>,
-      { name: "hello"; age: 20 }
-    >
-  >,
+  Expect<Equal<GetDataValue<{ data: 'hello' }>, 'hello'>>,
+  Expect<Equal<GetDataValue<{ data: { name: 'hello' } }>, { name: 'hello' }>>,
+  Expect<Equal<GetDataValue<{ data: { name: 'hello'; age: 20 } }>, { name: 'hello'; age: 20 }>>,
 ];
