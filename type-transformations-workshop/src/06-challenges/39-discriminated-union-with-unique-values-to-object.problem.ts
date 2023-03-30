28fa0443ddae5527f71d2e0a5b7345b115a665ad
RafaZeero <rafaa99@gmail.com>
2023-03-30 13:45:50 -0300

    solve challenge - discriminated union with unique values to object

--- type-transformations-workshop/src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
+++ type-transformations-workshop/src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
@@ -1,32 +1,41 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type Route =
   | {
-      route: "/";
+      route: '/';
       search: {
         page: string;
         perPage: string;
       };
     }
-  | { route: "/about" }
-  | { route: "/admin" }
-  | { route: "/admin/users" };
+  | { route: '/about' }
+  | { route: '/admin' }
+  | { route: '/admin/users' };
 
-type RoutesObject = unknown;
+type RoutesObject = {
+  [K in Route as K['route']]: K extends {
+    search: {
+      page: string;
+      perPage: string;
+    };
+  }
+    ? K['search']
+    : never;
+};
 
 type tests = [
   Expect<
     Equal<
       RoutesObject,
       {
-        "/": {
+        '/': {
           page: string;
           perPage: string;
         };
-        "/about": never;
-        "/admin": never;
-        "/admin/users": never;
+        '/about': never;
+        '/admin': never;
+        '/admin/users': never;
       }
     >
-  >,
+  >
 ];
