2cf88d2380927e6c5597c1217cad6d1e7174069e
RafaZeero <rafaa99@gmail.com>
2023-03-29 23:50:36 -0300

    solve discriminated union to object

--- type-transformations-workshop/src/05-key-remapping/33-discriminated-union-to-object.problem.ts
+++ type-transformations-workshop/src/05-key-remapping/33-discriminated-union-to-object.problem.ts
@@ -1,32 +1,34 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type Route =
   | {
-      route: "/";
+      route: '/';
       search: {
         page: string;
         perPage: string;
       };
     }
-  | { route: "/about"; search: {} }
-  | { route: "/admin"; search: {} }
-  | { route: "/admin/users"; search: {} };
+  | { route: '/about'; search: {} }
+  | { route: '/admin'; search: {} }
+  | { route: '/admin/users'; search: {} };
 
-type RoutesObject = unknown;
+type RoutesObject = {
+  [R in Route as R['route']]: R['search'];
+};
 
 type tests = [
   Expect<
     Equal<
       RoutesObject,
       {
-        "/": {
+        '/': {
           page: string;
           perPage: string;
         };
-        "/about": {};
-        "/admin": {};
-        "/admin/users": {};
+        '/about': {};
+        '/admin': {};
+        '/admin/users': {};
       }
     >
-  >,
+  >
 ];
