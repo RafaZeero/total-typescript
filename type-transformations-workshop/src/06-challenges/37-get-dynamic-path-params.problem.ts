868b396a3fe0537b3b7de2e07fa67ba5bb244c66
RafaZeero <rafaa99@gmail.com>
2023-03-30 11:58:37 -0300

    solve challenge - get dynamic path params

--- type-transformations-workshop/src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ type-transformations-workshop/src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,17 +1,24 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-type UserPath = "/users/:id";
+type UserPath = '/users/:id';
 
-type UserOrganisationPath = "/users/:id/organisations/:organisationId";
+type UserOrganisationPath = '/users/:id/organisations/:organisationId';
 
-type ExtractPathParams = unknown;
+type Split<Value extends string, Separator extends string> = Value extends string
+  ? Value extends `${infer Part1}${Separator}${infer Part2}`
+    ? [Part1, ...Split<Part2, Separator>]
+    : [Value]
+  : never;
+
+type ExtractPathParams<T extends string> = {
+  [K in Split<T, '/'>[number] as K extends `:${infer A}` ? A : never]: string;
+};
+
+type Test = ExtractPathParams<UserPath>;
+
+type Test2 = ExtractPathParams<UserOrganisationPath>;
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
-  Expect<
-    Equal<
-      ExtractPathParams<UserOrganisationPath>,
-      { id: string; organisationId: string }
-    >
-  >,
+  Expect<Equal<ExtractPathParams<UserOrganisationPath>, { id: string; organisationId: string }>>
 ];
